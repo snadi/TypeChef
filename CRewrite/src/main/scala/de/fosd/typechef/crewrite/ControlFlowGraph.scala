0544891da67245ce0d45a6d2e9525685b78a40a9
Jörg Liebig <joliebig@fim.uni-passau.de>
2011-08-03 14:34:48 +0200

    preliminary version of an conditional control flow graph

--- CRewrite/src/main/scala/de/fosd/typechef/crewrite/ControlFlowGraph.scala
+++ CRewrite/src/main/scala/de/fosd/typechef/crewrite/ControlFlowGraph.scala
@@ -1,5 +1,33 @@
 package de.fosd.typechef.crewrite
 
-class ControlFlowGraph {
+import org.kiama._
+import org.kiama.attribution.DynamicAttribution._
+import de.fosd.typechef.parser.c._
 
+// http://code.google.com/p/kiama/source/browse/src/org/kiama/example/dataflow/Dataflow.scala
+trait ControlFlow {
+  val succ: Statement ==> Set[Statement]
+  val following: Statement ==> Set[Statement]
 }
+
+trait ControlFlowImpl extends ControlFlow {
+  val succ: Statement ==> Set[Statement] =
+    attr {
+      case CompoundStatement(h :: _) => Set(h)
+      case EmptyStatement() => Set()
+      case IfStatement(_, thenBranch, elifs, elseBranch) => Set(thenBranch, elifs, elseBranch)
+      case t @ WhileStatement(_, s) => t->following + s
+    }
+
+  val following: Statement => Set[Statement] =
+    childAttr {
+      case s => {
+        case t @ IfStatement(_, _, _, _) => t->following
+        case t @ WhileStatement(_, _) => Set(t)
+        case b: CompoundStatement if s isLast => b->following
+        case CompoundStatement(_) => Set(s.next)
+        case _ => Set()
+      }
+    }
+}
+
