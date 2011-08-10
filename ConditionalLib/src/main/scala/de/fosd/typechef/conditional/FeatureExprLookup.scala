87b8d085156d92c7123fb83325f2aae62e0fc3e5
Jörg Liebig <joliebig@fim.uni-passau.de>
2011-08-10 14:51:38 +0200

    get rid of a couple of warnings: changed -> 2 ()

--- ConditionalLib/src/main/scala/de/fosd/typechef/conditional/FeatureExprLookup.scala
+++ ConditionalLib/src/main/scala/de/fosd/typechef/conditional/FeatureExprLookup.scala
@@ -13,10 +13,10 @@ trait FeatureExprLookup {
     val featureExpr: Attributable ==> FeatureExpr = attr {
         case node =>
             node.parent match {
-                case o@Opt(f, _) => o -> featureExpr and f
-                case c: Choice[_] => c -> featureExpr and (if (c.thenBranch == node) c.feature else c.feature.not)
+                case o@Opt(f, _) => featureExpr(o) and f
+                case c: Choice[_] => featureExpr(c) and (if (c.thenBranch == node) c.feature else c.feature.not)
                 case null => FeatureExpr.base
-                case e => e -> featureExpr
+                case e => featureExpr(e)
             }
 
 
