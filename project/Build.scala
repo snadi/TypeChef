7ec947138db1ef713bd9b9d7e4ba19cfdf32cdb9
snadi <snadi@uwaterloo.ca>
2013-03-25 15:40:31 -0400

    remove the maven publishing part because it wasn't making sbt publish-local work for some reason

--- project/Build.scala
+++ project/Build.scala
@@ -33,37 +33,7 @@ object BuildSettings {
         parallelExecution := false, //run into memory problems on hudson otherwise
 
         homepage := Some(url("https://github.com/ckaestne/TypeChef")),
-        licenses := Seq("GNU General Public License v3.0" -> url("http://www.gnu.org/licenses/gpl.txt")),
-
-        //maven
-        publishTo <<= version {
-            (v: String) =>
-                val nexus = "https://oss.sonatype.org/"
-                if (v.trim.endsWith("SNAPSHOT"))
-                    Some("snapshots" at nexus + "content/repositories/snapshots")
-                else
-                    Some("releases" at nexus + "service/local/staging/deploy/maven2")
-        },
-        publishMavenStyle := true,
-        publishArtifact in Test := false,
-        pomIncludeRepository := { _ => false },
-        pomExtra :=
-            <parent>
-                <groupId>org.sonatype.oss</groupId>
-                <artifactId>oss-parent</artifactId>
-                <version>7</version>
-            </parent> ++
-                <scm>
-                    <connection>scm:git:git@github.com:ckaestne/TypeChef.git</connection>
-                    <url>git@github.com:ckaestne/TypeChef.git</url>
-                </scm> ++
-                <developers>
-                    <developer>
-                        <id>ckaestne</id> <name>Christian Kaestner</name> <url>http://www.cs.cmu.edu/~ckaestne/</url>
-                    </developer>
-                </developers>,
-
-        credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+        licenses := Seq("GNU General Public License v3.0" -> url("http://www.gnu.org/licenses/gpl.txt"))        
     )
 }
 
